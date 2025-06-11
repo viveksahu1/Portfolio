288ff918d11adfed740bfdcb3332c970388eb9a5
Vivek Sahu <vivek.s@callify.ai>
2025-06-11 17:59:25 +0530

    routing issue

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // The base path should be the name of your GitHub repository.
-  base: '/Portfolio/', 
   
-  // This tells Vite to build the project into the 'docs' folder.
   build: {
     outDir: 'docs'
   },
