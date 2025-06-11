b4aea8665b6c57ba5f7e4a1778a3084a9df83160
Vivek Sahu <vivek.s@callify.ai>
2025-06-11 17:20:44 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// Replace 'your-repo-name' with the actual GitHub repo name
 export default defineConfig({
+  base: '/Portfolio/',  // 👈 Important for GitHub Pages
   plugins: [vue()],
   server: {
     port: 3000
