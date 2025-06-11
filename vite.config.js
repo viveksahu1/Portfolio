f30e64c507691160d0c742fb72bf5f3338e0e5a3
Vivek Sahu <vivek.s@callify.ai>
2025-06-11 18:21:58 +0530

    route

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // Add this line
-  base: '/Portfolio/',
+  // Add this line to fix the paths for GitHub Pages
+  base: '/Portfolio/', 
 })
